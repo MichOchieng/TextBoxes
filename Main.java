96bd8d2c966a0c71d9c5f2e9fd0d872301a64266
MichOchieng <MichOchieng@gmail.com>
2019-03-21 17:27:50 -0700

    Creates Frame object

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
-public class  Main{
-    public static void main(String[] args) {
-        System.out.println("first commit");
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main{
+        public static void main(String[] args) {
+        Frame main = new Frame();       
+        main.setVisible(true);
     }
 }
