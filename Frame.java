96e3ba1616a4088c5e69ce0c86faae14a3bd0f4f
MichOchieng <MichOchieng@gmail.com>
2019-03-21 17:27:08 -0700

    Added calculator logic \n Bug: Flagging non doubles doesn't work right

---
+++ Frame.java
@@ -0,0 +1,143 @@
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JComponent;
+import javax.swing.JButton;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class Frame extends JFrame{
+    public double num1;
+    public double num2;
+    public double num3;
+    public double num4; 
+    protected ArrayList inputs;   
+
+    public Frame(){
+        JPanel p = new JPanel();
+        p.setLayout(null);
+        setContentPane(p);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("Expenses");
+        setSize(550,550); 
+
+        // TEXT FIELDS
+        JTextField jt = new JTextField("EXPENSE",10);
+        JTextField jt2 = new JTextField();
+        JTextField jt3 = new JTextField("EXPENSE",10);
+        JTextField jt4 = new JTextField();
+        JTextField jt5 = new JTextField("EXPENSE",10);
+        JTextField jt6 = new JTextField();
+        JTextField jt7 = new JTextField("EXPENSE",10);
+        JTextField jt8 = new JTextField();
+        JTextField total = new JTextField("Total",10);
+        JTextField result = new JTextField();   
+
+        jt2.setToolTipText("Enter a number value");
+        jt4.setToolTipText("Enter a number value");
+        jt6.setToolTipText("Enter a number value");
+        jt8.setToolTipText("Enter a number value");
+        result.setToolTipText("This is your total");
+        // JTF - Positioning
+        jt.setBounds(100,150,100,20);
+        jt2.setBounds(200,150,150,20); 
+        jt3.setBounds(100,170,100,20);
+        jt4.setBounds(200,170,150,20);
+        jt5.setBounds(100,190,100,20);
+        jt6.setBounds(200,190,150,20); 
+        jt7.setBounds(100,210,100,20);
+        jt8.setBounds(200,210,150,20);
+        total.setBounds(100,270,100,20);
+        result.setBounds(200,270,150,20);
+
+        // JTF - Text Positioning
+        jt.setHorizontalAlignment(JTextField.CENTER);
+        jt3.setHorizontalAlignment(JTextField.CENTER);
+        jt5.setHorizontalAlignment(JTextField.CENTER);
+        jt7.setHorizontalAlignment(JTextField.CENTER);
+        total.setHorizontalAlignment(JTextField.CENTER);
+
+        jt.setEditable(false);
+        jt3.setEditable(false);
+        jt5.setEditable(false);
+        jt7.setEditable(false);
+        total.setEditable(false);
+        
+        // BUTTON
+        Font font = new Font("Comic Sans MS", Font.BOLD, 15);
+        JButton calculate = new JButton("Calculate");
+        calculate.setToolTipText("What are you waiting for, click me!");
+        calculate.setBounds(150,300,140,50);
+        calculate.setBackground(Color.WHITE);
+        calculate.setFont(font);
+        calculate.addActionListener(new ActionListener(){        
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    num1 = Double.parseDouble(jt2.getText());
+                    num2 = Double.parseDouble(jt4.getText());
+                    num3 = Double.parseDouble(jt6.getText());
+                    num4 = Double.parseDouble(jt8.getText());
+                    double x = num1+num2+num3+num4;
+                    result.setText(String.valueOf(x));
+                }
+                catch(InputMismatchException ex){
+                    System.out.println(ex.getMessage());  
+                     if(jt2.getText() instanceof String){
+                        jt2.setBackground(Color.YELLOW);
+                     }
+                     else if(jt4.getText() instanceof String){
+                        jt4.setBackground(Color.YELLOW);
+                     }  
+                     else if(jt6.getText() instanceof String){
+                        jt6.setBackground(Color.YELLOW);
+                     }  
+                     else if(jt8.getText() instanceof String){
+                        jt8.setBackground(Color.YELLOW);
+                     }                                         
+                }
+                catch(NumberFormatException ex){
+                    System.out.println(ex.getMessage());                      
+                     if(jt2.getText() instanceof String){
+                        jt2.setBackground(Color.YELLOW);
+                     }
+                     else if(jt4.getText() instanceof String){                         
+                        jt4.setBackground(Color.YELLOW);
+                     }  
+                     else if(jt6.getText() instanceof String){
+                        jt6.setBackground(Color.YELLOW);
+                     }  
+                     else if(jt8.getText() instanceof String){
+                        jt8.setBackground(Color.YELLOW);
+                     }                   
+                }                
+            }
+        });        
+        
+        // DISPLAY OUTPUT
+        p.add(jt);
+        p.add(jt2);
+        p.add(jt3);
+        p.add(jt4);
+        p.add(jt6);
+        p.add(jt7);
+        p.add(jt8);
+        p.add(jt5);
+        p.add(total);
+        p.add(result);
+        p.add(calculate);
+    }    
+}
+
+// try {
+//     Thread.sleep(2000);
+//     jt2.setBackground(Color.WHITE);
+// } catch (InterruptedException exs) {
+//     System.out.println(exs.getMessage());
+// }  
